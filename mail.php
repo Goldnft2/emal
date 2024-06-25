f6e963d2aa71fd3c353717b7cf2f4841a03d39de
Goldnft2 <weprogramador@gmail.com>
2024-06-25 16:18:02 -0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>envie seu email</title>
+</head>
+<body>
+    <?php
+    if(email(goldnftoficial@gmail.com, goldnftoficial@gmail.com, goldnftoficial@gmail.com, goldnftoficial@gmail.com)){
+        echo"email enviado com sucesso!"
+    }else {
+        echo"Holve um erro!"
+    }
+    ?>
+</body>
+</html>
